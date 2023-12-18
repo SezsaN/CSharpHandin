ac132ab79720dd5853812424a29e7cef8eb554b3
Andreas Fredriksson <afredriksson89@gmail.com>
2023-12-18 10:44:09 +0100

    Added Test to the project

---
+++ ConsoleAppHandin.Tests/ContactService_Tests.cs
@@ -0,0 +1,22 @@
+﻿using ClassLibrary.Handin.Interfaces;
+using ClassLibrary.Handin.Services;
+
+namespace ConsoleAppHandin.Tests;
+
+public class ContactService_Tests
+{
+    [Fact]
+    public void GetAllContactsFromListShould_GetAllContactsInContactList_ThenReturnListOfContacts()
+    {
+        // Arrange
+        IContactService contactService = new ContactService();
+
+        // Act
+        var result = contactService.GetAllContactsFromList();
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsType<List<IContact>>(result);
+ 
+    }
+}
