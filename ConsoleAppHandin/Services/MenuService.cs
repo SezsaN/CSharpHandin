361135f38ca3ec3ec21b1c16eb81114278aa1a86
Andreas Fredriksson <afredriksson89@gmail.com>
2023-12-18 09:35:23 +0100

    Added, delete, showAll and searchContact

--- ConsoleAppHandin/Services/MenuService.cs
+++ ConsoleAppHandin/Services/MenuService.cs
@@ -30,25 +30,79 @@ internal class MenuService : IMenuService
         contact.Email = Console.ReadLine()!;
         Console.Write("Enter address: ");
         contact.Address = Console.ReadLine()!;
-
+       
+ 
         _contacts.AddContactToList(contact);
+
+        Console.WriteLine();
+        Console.WriteLine("Contact added press any key to return to menu...");
     }
 
     private void ShowAllContactsMenu()
     {
-        throw new NotImplementedException();
+        DisplayMenuTitle("All Contacts in the list");
+        var res = _contacts.GetAllContactsFromList();
+
+        if (res != null)
+        {
+            foreach (var contact in res)
+            {
+                Console.WriteLine($"First Name: {contact.FirstName}");
+                Console.WriteLine($"Last Name: {contact.LastName}");
+                Console.WriteLine($"Phone Number: {contact.PhoneNumber}");
+                Console.WriteLine($"Email: {contact.Email}");
+                Console.WriteLine($"Address: {contact.Address}");
+                Console.WriteLine();
+            }
+        }
+        else
+        {
+            Console.WriteLine("No contacts in the list");
+        }
+
+        Console.WriteLine("Press any key to return to menu...");
     }
 
     private void ShowDeleteContactMenu()
     {
-        throw new NotImplementedException();
+        DisplayMenuTitle("Delete a contact by entering the contacts emailadress");
+
+        Console.Write("Enter email address: ");
+        var email = Console.ReadLine();
+
+        if (email != null)
+        {
+               var res = _contacts.DeleteContactFromList(email);
+
+            if (res != null)
+            {
+                Console.WriteLine($"First Name: {res.FirstName}");
+                Console.WriteLine($"Last Name: {res.LastName}");
+                Console.WriteLine($"Phone Number: {res.PhoneNumber}");
+                Console.WriteLine($"Email: {res.Email}");
+                Console.WriteLine($"Address: {res.Address}");
+                Console.WriteLine();
+                Console.WriteLine("Contact deleted press any key to return to menu...");
+               
+            }
+            else
+            {
+                Console.WriteLine("Contact not found press any key to return to menu...");
+
+            }   
+
+            
+
+        }
+
+
     }
 
     private void ShowExitMenu()
     {
         Console.Clear();
         Console.Write("Are you sure you want to exit the application? (y/n)");
-        var option = Console.ReadLine();
+        var option = Console.ReadLine()!;
 
         if (option.Equals("y", StringComparison.OrdinalIgnoreCase))
         {
@@ -70,7 +124,7 @@ internal class MenuService : IMenuService
             Console.WriteLine("1. Add Contact");
             Console.WriteLine("2. Search Contact");
             Console.WriteLine("3. Show All Contacts");
-            Console.WriteLine("4. Delete Contact");
+            Console.WriteLine("4. Delete Contact By Email");
             Console.WriteLine("5. Exit");
             Console.Write("\nSelect an option: ");
             var option = Console.ReadLine();
@@ -103,7 +157,35 @@ internal class MenuService : IMenuService
 
     private void ShowSearchContactMenu()
     {
-        throw new NotImplementedException();
+        DisplayMenuTitle("Search a contact by entering the contacts emailadress");
+
+        Console.Write("Enter email address: ");
+        var email = Console.ReadLine();
+
+        if (email != null)
+        {
+            var res = _contacts.GetContactFromList(email);
+
+            if (res != null)
+            {
+                Console.WriteLine($"First Name: {res.FirstName}");
+                Console.WriteLine($"Last Name: {res.LastName}");
+                Console.WriteLine($"Phone Number: {res.PhoneNumber}");
+                Console.WriteLine($"Email: {res.Email}");
+                Console.WriteLine($"Address: {res.Address}");
+                Console.WriteLine();
+
+            }
+            else
+            {
+                Console.WriteLine("Contact not found press any key to return to menu...");
+
+            }
+
+           
+        }
+
+        
     }
 
 
